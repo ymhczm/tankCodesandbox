4d05cc2c3cf3eb8209862836cd496fb2e7b4269a
jayan22 <jackie.yang@ingka.ikea.com>
2022-08-12 17:16:35 +0800

    feat: 初始化demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="copy('测试copy')">复制到剪切板</button>
+</template>
+<script setup lang="ts">
+import { useCopy } from "tankhooks";
+console.log(1)
+const copy = async(value:string)=>{
+  const result = await useCopy(value);
+  if(result==="SUCCESS") {
+      console.log("复制到剪切板成功")
+  } else if(result==="ERROR"){
+      console.log("复制到剪切板失败")
+  }
+}
+</script>
+<style scoped>
+
+</style>
